91d3c7ea57f5179becbd7920161174825886f10b
braidencutforth <braiden89@gmail.com>
2018-08-04 16:08:21 -0700

    Adding helpers

--- master.py
+++ master.py
@@ -1,7 +1,14 @@
 import sud2sat as makeCNF
+import sat2sud as solveSudoku
+import helpers
+import os
 
 def main():
     print "Starting script"
+    Puzzles = open('samplesudoku.txt', 'r')
+    if Puzzles is None:
+        helpers.eprint("Could not open puzzles file.")
+    
     
         
 
